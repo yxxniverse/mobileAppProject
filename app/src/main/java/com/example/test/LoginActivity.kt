98fcd94d3a7d8d7d350e876903e7f62be1505b41
yxxniverse <yooniverse@knu.ac.kr>
2021-11-16 16:21:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.test
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calendar)
+        }
+}
---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var editId : EditText;
+    lateinit var userId : String;
+    lateinit var loginBtn : Button;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginBtn = findViewById<Button>(R.id.login_btn)
+        loginBtn.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
