3dfece48be99342ec86237bb79f5deb71c0e89bb
yxxniverse <yooniverse@knu.ac.kr>
2021-11-24 17:15:15 +0900

    join/login 완료

--- app/src/main/java/com/example/test/LoginActivity.kt
+++ app/src/main/java/com/example/test/LoginActivity.kt
@@ -8,14 +8,65 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.calendar)
+        setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+        var idEditText= findViewById<EditText>(R.id.insert_id)
+        var pwdEditText = findViewById<EditText>(R.id.insert_pwd)
+        var loginBtn = findViewById<Button>(R.id.login_btn)
+        var signupBtn = findViewById<Button>(R.id.signup_btn)
+
+        loginBtn.setOnClickListener {
+            signIn(idEditText.text.toString(),pwdEditText.text.toString())
+        }
+        signupBtn.setOnClickListener {
+            startActivity(Intent(this,JoinActivity::class.java))
+        }
     }
 
+    public override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }
+
+    // 로그인
+    private fun signIn(email: String, password: String) {
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "로그인 성공",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        moveMainPage(auth?.currentUser)
+                    } else {
+                        Toast.makeText(
+                            baseContext, "로그인 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+
+    // 유저정보 넘겨주고 메인 액티비티 호출
+    fun moveMainPage(user: FirebaseUser?){
+        if( user!= null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
 }
+
