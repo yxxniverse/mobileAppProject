7831adf906202b6592fab752970dcb50c6a795bb
최호정 <hojung2801@knu.ac.kr>
2021-12-19 00:12:50 +0900

    listview

--- app/src/main/java/com/example/test/EditActivity.kt
+++ app/src/main/java/com/example/test/EditActivity.kt
@@ -1,27 +1,47 @@
 package com.example.test
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
-    var List = arrayListOf<ListViewItem>(
 
+    lateinit var ref : DatabaseReference
+    lateinit var listItem : ArrayList<ListViewItem>
+    lateinit var listview: ListView
 
-
-
-    )
+    val user = Firebase.auth.currentUser
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_edit) //위치
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val Adapter = ListViewAdapter(this, List)
-        listView.adapter = Adapter
+        listItem = arrayListOf()
+        listview = findViewById(R.id.listView)
+        ref = FirebaseDatabase.getInstance().getReference("외박신청")
+
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    listItem.clear()
+                    for (e in snapshot.children){
+                        val info = e.getValue(ListViewItem::class.java)
+                        listItem.add(info!!)
+                    }
+                    val adapter = ListViewAdapter(this@EditActivity, listItem)
+                    listview.adapter = adapter
+                }
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/test/ListViewItem.kt
+++ app/src/main/java/com/example/test/ListViewItem.kt
@@ -2,4 +2,5 @@ package com.example.test
 
 class ListViewItem (val destination: String, val from: String, val to: String) {
 
+    constructor():this("","",""){}
 }
