782925e3b9db0a0ba3ebbd26100f5e5366b89a50
최호정 <hojung2801@knu.ac.kr>
2021-12-18 21:12:19 +0900

    edit mid

--- app/src/main/java/com/example/test/EditActivity.kt
+++ app/src/main/java/com/example/test/EditActivity.kt
@@ -1,15 +1,27 @@
 package com.example.test
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_edit.*
 
 class EditActivity : AppCompatActivity() {
-    var dogList = arrayListOf<ListViewItem>()
+    var List = arrayListOf<ListViewItem>(
+
+
+
+
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val Adapter = ListViewAdapter(this, List)
+        listView.adapter = Adapter
     }
 }
