36d5bdf3533d0836614644fbc208b64a95e8ea71
yxxniverse <yooniverse@knu.ac.kr>
2021-12-19 03:26:20 +0900

    마이페이지/외박신청 완료

---
+++ app/src/main/java/com/example/test/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
