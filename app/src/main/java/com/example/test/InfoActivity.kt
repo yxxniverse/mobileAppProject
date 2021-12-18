c4a70c19bb5332303c42c36109a2520a741df5d8
yxxniverse <yooniverse@knu.ac.kr>
2021-12-19 03:25:41 +0900

    마이페이지/외박신청 완료

--- app/src/main/java/com/example/test/InfoActivity.kt
+++ app/src/main/java/com/example/test/InfoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,11 +14,20 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_form.*
+import kotlinx.android.synthetic.main.activity_info.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class InfoActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database : DatabaseReference
+    private var calendar = Calendar.getInstance()
+    private var year = calendar.get(Calendar.YEAR)
+    private var month = calendar.get(Calendar.MONTH)
+    private var day = calendar.get(Calendar.DAY_OF_MONTH)
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
@@ -34,6 +45,14 @@ class InfoActivity : AppCompatActivity() {
             var userBirthdayEditText = findViewById<EditText>(R.id.join_birthday)
             var userDormNameEditText = findViewById<EditText>(R.id.join_dorm)
             var userDormRoomEditText = findViewById<EditText>(R.id.join_dorm2)
+
+            join_birthday_btn.setOnClickListener {  //캘린더뷰 만들기
+                val datePickerDialog = DatePickerDialog(this, { _, year, month, day ->
+                    join_birthday.text =
+                        year.toString() + "-" + (month + 1).toString() + "-" + day.toString()
+                }, year, month, day)
+                datePickerDialog.show()
+            }
 
             saveInfoBtn.setOnClickListener {
                 UserInfo(
