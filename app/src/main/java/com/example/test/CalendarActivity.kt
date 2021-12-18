3c36ac322036b4bffa243a8c3d1fa9599ff40435
최호정 <hojung2801@knu.ac.kr>
2021-12-18 15:46:47 +0900

    calendar fix

--- app/src/main/java/com/example/test/CalendarActivity.kt
+++ app/src/main/java/com/example/test/CalendarActivity.kt
@@ -1,51 +1,41 @@
 package com.example.test
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.DatePicker
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_form.*
 import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
-    var date1String = ""
-    var date2String = ""
+
+    private var calendar = Calendar.getInstance()
+    private var year = calendar.get(Calendar.YEAR)
+    private var month = calendar.get(Calendar.MONTH)
+    private var day = calendar.get(Calendar.DAY_OF_MONTH)
 
     @RequiresApi(Build.VERSION_CODES.N)
+    @SuppressLint("SetText118n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
-        cal_btn1.setOnClickListener {
-            val cal = Calendar.getInstance()    //캘린더뷰 만들기
-            val dateSetListener =
-                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                    date1String = "${year} - ${month} - ${dayOfMonth}"
-                    date_show1.text = date1String
-                }
-            DatePickerDialog(
-                this,
-                dateSetListener,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)
-            ).show()
+
+        cal_btn1.setOnClickListener {  //캘린더뷰 만들기
+            val datePickerDialog = DatePickerDialog(this, { _, year, month, day ->
+                date_show1.text =
+                    year.toString() + "-" + (month + 1).toString() + "-" + day.toString()
+            }, year, month, day)
+            datePickerDialog.show()
         }
+
         cal_btn2.setOnClickListener {
-            val cal = Calendar.getInstance()    //캘린더뷰 만들기
-            val dateSetListener =
-                DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                    date2String = "${year} - ${month} - ${dayOfMonth}"
-                    date_show2.text = date2String
-                }
-            DatePickerDialog(
-                this,
-                dateSetListener,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)
-            ).show()
+            val datePickerDialog = DatePickerDialog(this, { _, year, month, day ->
+                date_show2.text =
+                    year.toString() + "-" + (month + 1).toString() + "-" + day.toString()
+            }, year, month, day)
+            datePickerDialog.show()
         }
     }
 }
